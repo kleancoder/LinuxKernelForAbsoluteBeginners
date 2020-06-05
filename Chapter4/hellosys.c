dea5ae16c251411827b36bdc2c04219e687f582a
kleancoder <mahbub.aaman.app@gmail.com>
2020-06-05 19:39:54 +0600

    HelloSys updated

--- Chapter4/hellosys.c
+++ Chapter4/hellosys.c
@@ -4,6 +4,6 @@
 // asmlinkage tells compiler to look on the CPU stack for the function parameters, instead of registers.
 asmlinkage long sys_hellosys(void) {
 
-	printk(“Hello New System Call!\n”);
+	printk("Hello New System Call !\n");
 	return 0;
 }
